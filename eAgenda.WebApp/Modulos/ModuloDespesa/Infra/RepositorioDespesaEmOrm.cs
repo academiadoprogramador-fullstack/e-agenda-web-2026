6d220e47025c7be829cbceed0a33933f0a3b9ba6
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-07-06 16:13:59 -0300

    Adiciona método para ordenação de Categorias de Despesa

--- eAgenda.WebApp/Modulos/ModuloDespesa/Infra/RepositorioDespesaEmOrm.cs
+++ eAgenda.WebApp/Modulos/ModuloDespesa/Infra/RepositorioDespesaEmOrm.cs
@@ -7,13 +7,56 @@ namespace eAgenda.WebApp.Modulos.ModuloDespesa.Infra;
 public sealed class RepositorioDespesaEmOrm(EAgendaDbContext dbContext) :
     RepositorioBaseEmOrm<Despesa>(dbContext), IRepositorioDespesa
 {
+    public override Despesa? SelecionarPorId(Guid idSelecionado)
+    {
+        Despesa? despesa = registros
+            .Include(d => d.Categorias)
+            .ThenInclude(c => c.Despesas)
+            .SingleOrDefault(d => d.Id == idSelecionado);
+
+        OrdenarCategorias(despesa);
+
+        return despesa;
+    }
+
     public override List<Despesa> SelecionarTodos()
     {
-        return registros
+        List<Despesa> despesas = registros
             .Include(d => d.Categorias)
             .ThenInclude(c => c.Despesas)
             .OrderByDescending(d => d.DataOcorrencia)
             .ThenBy(d => d.Descricao)
+            .ToList();
+
+        foreach (Despesa despesa in despesas)
+            OrdenarCategorias(despesa);
+
+        return despesas;
+    }
+
+    public override List<Despesa> Filtrar(Func<Despesa, bool> filtro)
+    {
+        List<Despesa> despesas = registros
+            .Include(d => d.Categorias)
+            .ThenInclude(c => c.Despesas)
+            .OrderByDescending(d => d.DataOcorrencia)
+            .ThenBy(d => d.Descricao)
+            .Where(filtro)
+            .ToList();
+
+        foreach (Despesa despesa in despesas)
+            OrdenarCategorias(despesa);
+
+        return despesas;
+    }
+
+    private static void OrdenarCategorias(Despesa? despesa)
+    {
+        if (despesa == null)
+            return;
+
+        despesa.Categorias = despesa.Categorias
+            .OrderBy(c => c.Titulo)
             .ToList();
     }
 }
