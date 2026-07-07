57da3fce8255d489e2443162254fe9fb0ce8ed19
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-07-07 15:56:10 -0300

    Adiciona health check do DbContext

--- eAgenda.WebApp/Program.cs
+++ eAgenda.WebApp/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using eAgenda.WebApp.Compartilhado.Aplicacao;
 using eAgenda.WebApp.Compartilhado.Apresentacao;
 using eAgenda.WebApp.Compartilhado.Infra;
+using eAgenda.WebApp.Compartilhado.Infra.Orm;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +13,20 @@ builder.Services.AddApplicationServices(builder.Configuration, builder.Logging);
 
 builder.Services.AddPresentationConfig(builder.Configuration);
 
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<EAgendaDbContext>(
+        name: "database_check",
+        failureStatus: HealthStatus.Unhealthy,
+        tags: ["ready"]
+    );
+
 var app = builder.Build();
 
 // Middlewares de roteamento
 app.UseRouting();
 app.MapDefaultControllerRoute();
+
+app.MapHealthChecks("/health");
 
 // Execução do Servidor
 app.Run();
